3062436cc6d12e87c01f873f524e32aab53b12a0
David Johnston <dwtj@iastate.edu>
2015-06-01 17:57:26 -0500

    Initial commit.
    Includes a simple maven project which errors when the `NullnessChecker`
    detects that a `@NonNull` variable is assigned a `null`.

---
+++ src/main/java/me/dwtj/WeatherTracker.java
@@ -0,0 +1,10 @@
+package me.dwtj;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class WeatherTracker
+{
+    void obtainForecast() {
+        @NonNull Object ref = null;
+    }
+}
